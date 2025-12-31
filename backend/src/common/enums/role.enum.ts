33c4812e2f5cb6377674a769d78aa9dc5f4b9cbe
Hamza Shahab <155806025+hamzashahab7026@users.noreply.github.com>
2025-12-31 19:14:20 +0500

    owais backend

--- backend/src/common/enums/role.enum.ts
+++
@@ -1,5 +0,0 @@
-export enum Role {
-  ADMIN = 'ADMIN',
-  MEMBER = 'MEMBER',
-  VIEWER = 'VIEWER',
-}
--- backend/src/work-items/dto/update-work-item.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/swagger';
-import { CreateWorkItemDto } from './create-work-item.dto';
-
-export class UpdateWorkItemDto extends PartialType(CreateWorkItemDto) {}
